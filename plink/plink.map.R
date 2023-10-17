9b6f060f0f71997bc9270fb5d24617a93e66134c
Guohongping <51486558+Guohongping@users.noreply.github.com>
2023-10-17 15:54:34 +0800

    Add files via upload

---
+++ plink/plink.map.R
@@ -0,0 +1,11 @@
+plink.map <- function(q){
+  map1 <- matrix(0,q,1)
+  colnames(map1) <- "chr"
+  map2 <- matrix(1:nrow(map1),nrow(map1),1)
+  colnames(map2) <- "marker name"
+  map3 <- matrix("NA",q,1)
+  colnames(map3) <- "genetic distance"
+  map4 <- matrix(1:nrow(map1),nrow(map1),1)
+  colnames(map4) <- "position"
+  map <- cbind(map1,map2,map3,map4)
+}
---
+++ plink/plink.ph.R
@@ -0,0 +1,6 @@
+plink.ph <- function(n,phen,p){
+  ph3 <- phen
+  colnames(ph3) <- matrix(1:p,p,1)   
+  ph <- ph3
+  return(ph)
+}
