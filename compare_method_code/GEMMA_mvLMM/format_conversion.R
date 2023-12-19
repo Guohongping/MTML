2e8e6a4ae9a66f7efff7074bf3c184fdb3da8db3
Guohongping <51486558+Guohongping@users.noreply.github.com>
2023-12-19 20:16:01 +0800

    Update format_conversion.R

--- compare_method_code/GEMMA_mvLMM/format_conversion.R
+++ compare_method_code/GEMMA_mvLMM/format_conversion.R
@@ -1,8 +1,8 @@
-source("./MTML_code_data/compare_method_code/GEMMA_mvLMM/plink.map.R")
-source("./MTML_code_data/compare_method_code/GEMMA_mvLMM/plink.ped.R")
-source("./MTML_code_data/compare_method_code/GEMMA_mvLMM/plink.ph.R")
+source("./compare_method_code/GEMMA_mvLMM/plink.map.R")
+source("./compare_method_code/GEMMA_mvLMM/plink.ped.R")
+source("./compare_method_code/GEMMA_mvLMM/plink.ph.R")
 
-genotype <- read.csv("./MTML_code_data/data/simu_data/snps.csv",header=T)
+genotype <- read.csv("./data/simu_data/snps.csv",header=T)
 genotype <- as.matrix(genotype[,4:202])
 genotype[genotype==0] <- "11"
  genotype[genotype==1] <- "12"
@@ -15,7 +15,7 @@ for (k in 1:nrow(trait_info)){
   for (r in 1:3){
     trait_k <- trait_info[k,1]
     N <- trait_info[k,2]
-    fname <- paste("./MTML_code_data/data/simu_data/simu_phenotype/Y",r,"_",trait_k,".csv",sep="")
+    fname <- paste("./data/simu_data/simu_phenotype/Y",r,"_",trait_k,".csv",sep="")
     phenotype <- read.csv(fname,header = T)
     phenotype <- as.matrix(phenotype[,-1])
 	phen <- phenotype
@@ -27,9 +27,9 @@ for (k in 1:nrow(trait_info)){
     peddh <- plink.ped(n,phen,gene,p,q)
 	phh <- plink.ph(n,phen,p)
 	
-	map_fname <- paste("./MTML_code_data/compare_method_code/GEMMA_mvLMM/y",r,"_",trait_k,".map",sep="")
-	ped_fname <- paste("./MTML_code_data/compare_method_code/GEMMA_mvLMM/y",r,"_",trait_k,".ped",sep="")
-	ph_fname <-  paste("./MTML_code_data/compare_method_code/GEMMA_mvLMM/y",r,"_",trait_k,".txt",sep="")
+	map_fname <- paste("./compare_method_code/GEMMA_mvLMM/y",r,"_",trait_k,".map",sep="")
+	ped_fname <- paste("./compare_method_code/GEMMA_mvLMM/y",r,"_",trait_k,".ped",sep="")
+	ph_fname <-  paste("./compare_method_code/GEMMA_mvLMM/y",r,"_",trait_k,".txt",sep="")
 	write.table(mapph,map_fname,row.names=F,col.names=F,quote=F)
 	write.table(peddh,ped_fname,row.names=F,col.names=F,quote=F)
 	write.table(phh,ph_fname,row.names=F,col.names=F,quote=F)
