575b9f40410dcf18710160556bab4d45787aab43
Guohongping <51486558+Guohongping@users.noreply.github.com>
2023-12-19 15:21:08 +0800

    Update MTML-master.R

--- code/MTML-master.R
+++ code/MTML-master.R
@@ -9,12 +9,12 @@ library(iterators)
 library(doParallel)
 library(bigmemory)
 
-source("./MTML_code_data/data/simu_data/simulate.R")
-source("./MTML_code_data/code/single_trait.R")
-source("./MTML_code_data/code/cauchyComb.R")
-source("./MTML_code_data/code/ridge.R")
-source("./MTML_code_data/code/MTML.R")
-source("./MTML_code_data/code/statistic.R")
+source("./data/simu_data/simulate.R")
+source("./code/single_trait.R")
+source("./code/cauchyComb.R")
+source("./code/ridge.R")
+source("./code/MTML.R")
+source("./code/statistic.R")
 
 kk          <-   NULL                 
 psmatrix    <-   NULL           
@@ -28,7 +28,7 @@ spvalue     <-   0.05
 #########################################################################################################
 ### Simulation
 
-QTN              <-  read.csv("/home/ghp/litong/MTML2024/MTML_code_data/data/simu_data/QTNs.csv",header = F)  # four QTNs
+QTN              <-  read.csv("./data/simu_data/QTNs.csv",header = F)  # four QTNs
 trait_info       <-  matrix(c(2, 5, 10, 500, 200, 100), 3, 2) 
 genetic_effect1  <-  matrix(c(2.9161, 2.6082, 2.2588, 1.3041), nrow=4, ncol=1) 
 genetic_effect2  <-  matrix(c(3.1944, 2.8571, 2.4743, 1.4286), nrow=4, ncol=1)
@@ -40,7 +40,7 @@ simu_data(QTN, trait_info, genetic_effect1, genetic_effect2)
 ### MTML Method
          
  
-genotype     <-  read.csv("/home/ghp/litong/MTML2024/MTML_code_data/data/simu_data/snps.csv",header=T)
+genotype     <-  read.csv("./data/simu_data/snps.csv",header=T)
 gen          <-  cbind(as.matrix(genotype[1:4000,1:3]),as.matrix(genotype[1:4000,4:202]))
 trait_info   <-  matrix(c(2, 5, 10, 500, 200, 100), 3, 2)
 
@@ -49,7 +49,7 @@ for (k in 1:nrow(trait_info)){
   for (r in 1:3){
     trait_k      <-  trait_info[k,1]
     N            <-  trait_info[k,2]
-    fname        <-  paste("/home/ghp/litong/MTML2024/MTML_code_data/data/simu_data/simu_phenotype/Y",r,"_",trait_k,".csv",sep="")
+    fname        <-  paste("./data/simu_data/simu_phenotype/Y",r,"_",trait_k,".csv",sep="")
     phenotype    <-  read.csv(fname,header = T)
     phenotype    <-  as.matrix(phenotype[,-1])
     matrix_final <-  MTML(gen, phenotype, kk, psmatrix, N, svpal, Genformat, Likelihood, CLO, trait_k, snp)
@@ -58,4 +58,4 @@ for (k in 1:nrow(trait_info)){
 	MTML_perform <-  rbind(MTML_perform,statis)
   }
 }
-write.csv(MTML_perform,"/home/ghp/litong/MTML2024/MTML_code_data/results/MTML_result.csv")
+write.csv(MTML_perform,"./results/MTML_result.csv")
